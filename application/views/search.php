153711b7af48c3313e26e2a44cf8d35514b8d2c3
unknown <c.kunapakornkarn@hotmail.com>
2018-10-11 02:11:06 +0700

    fondend sus but not friendly

--- application/views/search.php
+++ application/views/search.php
@@ -7,17 +7,97 @@
         <h3>ข้อมูลของอาหารไทยปรุงรสสำเร็จและอาหารจานเดี่ยว</h2><br>
         <img src="https://www.picz.in.th/images/2018/10/05/hwNyxf.png" alt="hwNyxf.png" border="0"><br><br>
         <p>Home &diams; Search</p>
-        <form class="example" action="/action_page.php" style="margin:auto;max-width:300px">
-        <input type="text"><br><br>
+        <form class="example" id="form" style="margin:auto;max-width:300px">
+        <input id="name" type="text"><br><br>
         <center>
         <button class="button" style="vertical-align:middle"><span><font color="#FFFFFF" id="search">SEARCH</span></button>
         </form>
         </center>
         </th>
-        
-
+</table>
+<table class="table table-bordered" id="result">
 </table>
 
+<script>
+    $(document).ready(function(){
+        
+        $("#form").submit(function(e){
+            e.preventDefault();
+            // var formdata = $(this).serializeObject();
+            // console.log(formdata);
+            var formdata = {
+                "thname" : $("#name").val(),
+                "enname" : $('#name').val()
+            }
+            let result = $("#result");
+            $.post("http://localhost:8080/Nutritional/api/nutritional/search", JSON.stringify(formdata),
+                function (data, textStatus, jqXHR) {
+                    var listOfResult = data.data;
+                     var strResult = '<thead>'
+                                    +'<tr class="info">'
+                                    +'<th colspan="2"><center>ชื่อ</center></th>'
+                                    +'<th ><center>พลังงาน</th>'
+                                    +'<th ><center>น้ำ</th>'
+                                    +'<th ><center>โปรตีน</th>'
+                                    +'<th ><center>ไขมัน</th>'
+                                    +'<th ><center>คาร์โบไฮเดรต</th>'
+                                    +'<th ><center>ใยอาหาร/(เส้นใยดิบ)</th>'
+                                    +'<th ><center>เถ้า</th>'
+                                    +'<th ><center>แคลเซียม</th>'
+                                    +'<th ><center>ฟอสฟอรัส</th>'
+                                    +'<th ><center>ธาตุเหล็ก</th>'
+                                    +'<th ><center>เรตินอย</th>'
+                                    +'<th ><center>เบต้าคาโรทีน</th>'
+                                    +'<th ><center>วิตามินA</th>'
+                                    +'<th ><center>วิตามินE</th>'
+                                    +'<th ><center>ธิเอมิน</th>'
+                                    +'<th ><center>ลิโบฟลาวิน</th>'
+                                    +'<th ><center>นีเอซิน</th>'
+                                    +'<th ><center>วิตามินC</th>'
+                                    +'</tr>'
+                                    +'<tr class="info">'
+                                    +'<th><center>ชื่อไทย</th>'
+                                    +'<th><center>ชื่ออังกฤษ</th>'
+                                    +'<th><center>กิโลแคลลอลี่</th>'
+                                    +'<th colspan="6"><center>กรัม</center> </th>'
+                                    +'<th colspan="3"><center>มิลลิกรัม</center> </th>'
+                                    +'<th colspan="3"><center>ไมโครกรัม</center> </th>'
+                                    +'<th colspan="5"><center>มิลลิกรัม</center> </th>'
+                                    +'</tr>'
+                                    +'</thead>';
+                    $.each(listOfResult , function (index ,value){
+                        strResult += '<tr><td class="text-center">'+ value.thname+'</td>'
+                                    +'<td class="text-center">'+ value.enname+'</td>'
+                                    +'<td class="text-center">'+ value.composition.energy+'</td>'
+                                    +'<td class="text-center">'+ value.composition.water+'</td>'
+                                    +'<td class="text-center">'+ value.composition.protein+'</td>'
+                                    +'<td class="text-center">'+ value.composition.fat+'</td>'
+                                    +'<td class="text-center">'+ value.composition.carbohydrate+'</td>'
+
+                                    +'<td class="text-center" >'+ value.composition.fiber+'</td>'
+                                    +'<td class="text-center">'+ value.composition.ash+'</td>'
+                                    +'<td class="text-center">'+ value.mineral.calsium+'</td>'
+                                    +'<td class="text-center">'+ value.mineral.phosphorus+'</td>'
+                                    +'<td class="text-center">'+ value.mineral.iron+'</td>'
+                                    +'<td class="text-center" >'+ value.vitamin.retinol+'</td>'
+                                    +'<td class="text-center">'+ value.vitamin.betacarotene+'</td>'
+                                    +'<td class="text-center">'+ value.vitamin.vitaminA+'</td>'
+                                    +'<td class="text-center">'+ value.vitamin.vitaminE+'</td>'
+                                    +'<td class="text-center">'+ value.vitamin.thiamin+'</td>'
+                                    +'<td class="text-center">'+ value.vitamin.riboflavin+'</td>'
+                                    +'<td class="text-center">'+ value.vitamin.niacin+'</td>'
+                                    +'<td class="text-center">'+ value.vitamin.vitaminC+'</td>'
+                    });
+                    result.html(strResult);
+                    // alert(data.message);
+                }    
+            );
+            
+        });
+    });
+   
+
+</script>
 <!--
 <div class="container">
 
