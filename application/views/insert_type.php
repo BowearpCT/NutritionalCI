dcb86257b51cca699f9872dc8c88ce808b53aff6
unknown <fill9029f@gmail.com>
2018-10-12 15:07:06 +0700

    insert type

--- application/views/insert_type.php
+++ application/views/insert_type.php
@@ -1,21 +1,108 @@
+<script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+    crossorigin="anonymous"></script>
 
+<table class="table table-bordered" >
+<tr class="w3-hover-#f1f1f1">
 
+        <th bgcolor="#f0f5f5">
+        <center>
+        <h2 >หมวดหมู่อาหาร</h2>
+        <h3>หมวดหมู่อาหารของอาหารไทยแต่ละชนิด</h2><br>
+        <img src="https://img.live/images/2018/10/10/broccoli1.png" alt="broccoli1.png" border="0" />
+        <img src="https://img.live/images/2018/10/10/meat.png" alt="meat.png" border="0" />
+        <img src="https://img.live/images/2018/10/10/orange1.png" alt="orange1.png" border="0" />
+        <img src="https://img.live/images/2018/10/10/soybean.png" alt="soybean.png" border="0" />
+        <img src="https://img.live/images/2018/10/10/fish.png" alt="fish.png" border="0" /><br><br>
+        <p>Home &diams; Type Of Foods</p>
+        <form id="insert" class="example"  style="margin:auto;max-width:300px">
+        <input  type="text" placeholder="Enter Type" name="nametype"  id="nametype" required><br><br>
+        <center>
+        <button class="button"type="submit" style="vertical-align:middle"><span><font color="#FFFFFF" >INSERT</span></button>
+        </form>
+        </center>
+        </th>
+</table>
 
-<form >
-<h2 align="center">Insert Type</h2>
-  <div class="imgcontainer">
-    <img src="<?php base_url(); ?>../assets/images/02-28.jpg" alt="Avatar" class="avatar">
-  </div>
+<!-- insert -->
+<script>
+    $(document).ready(function(){
+        $("#insert").submit(function(e){
+                e.preventDefault();
+                // var formdata = $(this).serializeObject();
+                // console.log(formdata);
+                var formdata = {
+                    "nametype" : $("#nametype").val(), 
+                }
+                $.post("http://localhost:8080/Nutritional/api/type/insert", JSON.stringify(formdata),
+                    function (data, textStatus, jqXHR) {
+                        alert(data.message);
+                    }    
+                );
+            });
+    });
+</script>
 
-  <div class="container">
-    <label for="uname"><b>ID</b></label>
-    <input type="text"  placeholder="Enter ID" name="id" required><br>
+<!-- table -->
 
-    <label for="psw"><b>Type</b></label>
-    <input type="text" placeholder="Enter Type" name="type" required>
-        
-    <button type="submit">Insert</button>
-    </div>
-</form>
+<div class="container" center>
+  <h2>Type Of Foods</h2>          
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>NameType</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody id="type">
+    </tbody>
+  </table>
+</div>
 
+<!-- findAll -->
+<script>
+        let type = $("#type");
+        let i = 1;
+        $.post("http://localhost:8080/Nutritional/api/type/findAll/", {},
+            function (data, textStatus, jqXHR) {
+                console.log(data);
+                var listOfherb = data.data;
+                var strType = "";
+                $.each(listOfherb, function( index, value ) {
+                    strType += '<tr>'
+                            + '<td>'+i+'</td>'
+                            + '<td>'+value.name+'</td>'
+                            + '<td>'
+                            +'<a href="editype/'+value._id+'/'+value.nametype+'/'+value.warning+'" class="btn btn-info">'
+                            + '<i class="far fa-edit"></i> Edit'
+                            + '</a> &nbsp;'
+                            + '<button class="btn btn-danger" oneclick+"deleteType"(\''+value._id+'\')">'
+                            + '<i class="far fa-edit"></i> Delete'
+                            + '<butoon>'
+                            + '</td>'
+                            + '</tr>';
+                            i++;            
+                });
+                type.html(strType);  
+            }
+        );
+    </script>
 
+<!-- dalete
+<script>
+function delete1(typeid){
+  $.ajax({
+    type: "delete",
+    url: "http://localhost:8080/Nutritional/api/delete/",
+    data: JSON class="stringify",
+    dataType: "dataType",
+    success: function (response) {
+    }
+  });
+}
+
+</script> -->
