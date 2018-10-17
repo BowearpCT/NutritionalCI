53b72ccc8e1ebaf16cf814f3ada44f4f50a65cef
unknown <bestnacufzxzx1@gmail.com>
2018-10-17 18:55:46 +0700

    edit form nu

--- application/views/cal_nutrition/content.php
+++ application/views/cal_nutrition/content.php
@@ -13,196 +13,217 @@
 
 <br>
 <br>
-<form id="form-calculat">
-  <div class="container">
-    <div class="col-md-2">
-      <button type="button" class="btn btn-primary col-md-4 btn-block ">ส่ง</button>
-    </div>
-<br>
-    <div class="row">    
+<div class="container">
+    <br>
+    <div class="row">     
       <div class="col-sm-6"> <!-- //ช่อง1// -->
-          <div class="row">
-            <div class="col-md-6">
-            <label>ธัญพืชและผลิตภัณฑ์</label>
-              <input type="text" class="form-control" id="products"  placeholder="ธัญพืช" style="color: #0000FF" >
-            </div>
-            <div class="col-md-5">
-            <label>ปริมาณ</label>                
-              <div class="input-group">
-                <input type="text" class="form-control" id="productscal"  placeholder="กรัม">
-                  <span class="input-group-btn">
-                    <button class="btn  glyphicon glyphicon-plus" type="button" id="create_one"></button>
-                  </span>
-              </div>
-            </div>
-          </div>
-          <span id="temp_one"></span>
 
-
-          <div class="row">
-            <div class="col-md-6">
-            <label>รากและหัวของพืช</label>
-              <input type="text" class="form-control" id="plants"  placeholder="รากและหัวของพืช">
-            </div>
-            <div class="col-md-5">
-            <label>ปริมาณ</label>                
-              <div class="input-group">
-                <input type="text" class="form-control" id="plantscal"  placeholder="กรัม">  
-                  <span class="input-group-btn">
-                    <button class="btn  glyphicon glyphicon-plus" type="button" id="create_two"></button>
-                  </span>
-              </div>
-            </div>
-          </div>
-          <span id="temp_two"></span>
-
-
-          <div class="row">
-            <div class="col-md-6">
-            <label>ผลไม้เปลือกแข็ง พืชเมล็ด</label>
-              <input type="text" class="form-control" id="seedlings"  placeholder="ผลไม้เปลือกแข็ง">
-            </div>
-            <div class="col-md-5">
-            <label>ปริมาณ</label>                
-              <div class="input-group">
-                <input type="text" class="form-control" id="seedlingscal"  placeholder="กรัม">  
-                  <span class="input-group-btn">
-                    <button class="btn  glyphicon glyphicon-plus" type="button" id="create_three"></button>
-                  </span>
-              </div>
-            </div>
-          </div>
-          <span id="temp_three"></span>
-
-          <div class="row">
-            <div class="col-md-6">
-            <label>ผัก</label>
-              <input type="text" class="form-control" id="vegetable"  placeholder="ผัก"> 
-            </div>
-            <div class="col-md-5">
-            <label>ปริมาณ</label>                
-              <div class="input-group">
-                <input type="text" class="form-control" id="vegetablecal"  placeholder="กรัม"> 
-                  <span class="input-group-btn">
-                    <button class="btn  glyphicon glyphicon-plus" type="button" id="create_four"></button>
-                  </span>
-              </div>
-            </div>
-          </div>
-          <span id="temp_four"></span>
-
-          <div class="row">
-            <div class="col-md-6">
-              <label>ผลไม้</label>
-                <input type="text" class="form-control" id="fruit"  placeholder="ผลไม้">
-              </div>
-            <div class="col-md-5">
-            <label>ปริมาณ</label>                
-              <div class="input-group">
-                <input type="text" class="form-control" id="fruitcal"  placeholder="กรัม">
-                  <span class="input-group-btn">
-                    <button class="btn  glyphicon glyphicon-plus" type="button" id="create_five"></button>
-                  </span>
-              </div>
-            </div>
-          </div>
-          <span id="temp_five"></span>
-
-      </div>
-      
-      <div class="col-sm-6" ><!-- //ช่อง2// -->
-          <div class="row">
-            <div class="col-md-6">
-            <label>เนื้อสัตว์</label>
-              <input type="text" class="form-control" id="meat" placeholder="เนื้อสัตว์"> 
-            </div>
-            <div class="col-md-5">
-            <label>ปริมาณ</label>                
-              <div class="input-group">
-                <input type="text" class="form-control" id="meatcal" placeholder="กรัม"> 
-                  <span class="input-group-btn">
-                    <button class="btn  glyphicon glyphicon-plus" type="button" id="create_six"></button>
-                  </span>
-              </div>
-            </div>
-          </div>
-          <span id="temp_six"></span>
-
-          <div class="row">
-            <div class="col-md-6">
-              <label>สัตว์น้ำ</label>
-                <input type="text" class="form-control" id="aquatic" placeholder="สัตว์น้ำ">
+        <form id="cerealscal">  
+            <div class="row">
+              <div class="col-md-6">
+              <label>ธัญพืชและผลิตภัณฑ์</label>
+                <input type="text" class="form-control"  placeholder="ธัญพืช" name="cereals" style="color: #0000FF"  required >
               </div>
               <div class="col-md-5">
               <label>ปริมาณ</label>                
                 <div class="input-group">
-                  <input type="text" class="form-control" id="aquaticcal"  placeholder="กรัม">
+                  <input type="text" class="form-control" name="cerealscal" placeholder="กรัม" style="color: #0000FF" required>
                     <span class="input-group-btn">
-                      <button class="btn  glyphicon glyphicon-plus" type="button" id="create_seven"></button>
+                      <!-- <button class="btn  glyphicon glyphicon-plus" type="button" id="create_one"></button> -->
+                      <button type="submit" class="btn btn-primary col-md-4 btn-block ">ส่ง</button>
                     </span>
                 </div>
               </div>
             </div>
-            <span id="temp_seven"></span>
+            <!-- <span id="temp_one"></span> -->
+        </form>
 
+          <form id="namecal">  
+            <div class="row">
+              <div class="col-md-6">
+              <label>รากและหัวของพืช</label>
+                <input type="text" class="form-control" name="plants"  placeholder="รากและหัวของพืช" style="color: #0000FF"  required>
+              </div>
+              <div class="col-md-5">
+              <label>ปริมาณ</label>                
+                <div class="input-group">
+                  <input type="text" class="form-control" name="plantscal"  placeholder="กรัม" style="color: #0000FF"  required>  
+                    <span class="input-group-btn">
+                      <!-- <button class="btn  glyphicon glyphicon-plus" type="button" id="create_two"></button> -->
+                      <button type="submit" class="btn btn-primary col-md-4 btn-block " >ส่ง</button>
+                    </span>
+                </div>
+              </div>
+            </div>
+            <!-- <span id="temp_two"></span> -->
+          </form>
+
+          <form id="namecal">
+            <div class="row">
+              <div class="col-md-6">
+              <label>ผลไม้เปลือกแข็ง พืชเมล็ด</label>
+                <input type="text" class="form-control" name="seedlings"  placeholder="ผลไม้เปลือกแข็ง"  style="color: #0000FF"  required>
+              </div>
+              <div class="col-md-5">
+              <label>ปริมาณ</label>                
+                <div class="input-group">
+                  <input type="text" class="form-control" name"seedlingscal"  placeholder="กรัม"  style="color: #0000FF"  required>  
+                    <span class="input-group-btn">
+                      <!-- <button class="btn  glyphicon glyphicon-plus" type="button" id="create_three"></button> -->
+                      <button type="submit" class="btn btn-primary col-md-4 btn-block " >ส่ง</button>
+                    </span>
+                </div>
+              </div>
+            </div>
+            <!-- <span id="temp_three"></span> -->
+          </form>
+
+          <form id="namecal">
+            <div class="row">
+              <div class="col-md-6">
+              <label>ผัก</label>
+                <input type="text" class="form-control" name="vegetable"  placeholder="ผัก"  style="color: #0000FF"  required> 
+              </div>
+              <div class="col-md-5">
+              <label>ปริมาณ</label>                
+                <div class="input-group">
+                  <input type="text" class="form-control" name="vegetablecal"  placeholder="กรัม"  style="color: #0000FF"  required> 
+                    <span class="input-group-btn">
+                      <!-- <button class="btn  glyphicon glyphicon-plus" type="button" id="create_four"></button> -->
+                      <button type="submit" class="btn btn-primary col-md-4 btn-block " >ส่ง</button>
+                    </span>
+                </div>
+              </div>
+            </div>
+            <!-- <span id="temp_four"></span> -->
+          </form>
+
+          <form id="namecal">
+            <div class="row">
+              <div class="col-md-6">
+                <label>ผลไม้</label>
+                  <input type="text" class="form-control" name="fruit"  placeholder="ผลไม้"  style="color: #0000FF"  required>
+                </div>
+              <div class="col-md-5">
+              <label>ปริมาณ</label>                
+                <div class="input-group">
+                  <input type="text" class="form-control" name="fruitcal"  placeholder="กรัม"  style="color: #0000FF"  required>
+                    <span class="input-group-btn">
+                      <!-- <button class="btn  glyphicon glyphicon-plus" type="button" id="create_five"></button> -->
+                      <button type="submit" class="btn btn-primary col-md-4 btn-block " id="create_five">ส่ง</button>
+                    </span>
+                </div>
+              </div>
+            </div>
+            <!-- <span id="temp_five"></span> -->
+          </form>
+
+        </div>
+      
+      <div class="col-sm-6" ><!-- //ช่อง2// -->
+      
+      <form id="namecal">
+          <div class="row">
+            <div class="col-md-6">
+            <label>เนื้อสัตว์</label>
+              <input type="text" class="form-control" name="meat" placeholder="เนื้อสัตว์"  style="color: #0000FF"  required> 
+            </div>
+            <div class="col-md-5">
+            <label>ปริมาณ</label>                
+              <div class="input-group">
+                <input type="text" class="form-control" name="meatcal" placeholder="กรัม"  style="color: #0000FF"  required> 
+                  <span class="input-group-btn">
+                    <!-- <button class="btn  glyphicon glyphicon-plus" type="button" id="create_six"></button> -->
+                    <button type="submit" class="btn btn-primary col-md-4 btn-block " id="create_six">ส่ง</button>
+                  </span>
+              </div>
+            </div>
+          </div>
+          <!-- <span id="temp_six"></span> -->
+      </form>
+      
+      <form id="namecal">
+          <div class="row">
+            <div class="col-md-6">
+              <label>สัตว์น้ำ</label>
+                <input type="text" class="form-control" name="aquatic" placeholder="สัตว์น้ำ"  style="color: #0000FF"  required>
+              </div>
+              <div class="col-md-5">
+              <label>ปริมาณ</label>                
+                <div class="input-group">
+                  <input type="text" class="form-control" name="aquaticcal"  placeholder="กรัม"  style="color: #0000FF"  required>
+                    <span class="input-group-btn">
+                      <!-- <button class="btn  glyphicon glyphicon-plus" type="button" id="create_seven"></button> -->
+                      <button type="submit" class="btn btn-primary col-md-4 btn-block " id="create_seven">ส่ง</button>
+                    </span>
+                </div>
+              </div>
+            </div>
+            <!-- <span id="temp_seven"></span> -->
+      </form>
+
+      <form id="namecal">
              <div class="row">
               <div class="col-md-6">
                 <label>ไข่</label>
-                  <input type="text" class="form-control" id="egg" placeholder="ไข่"> 
+                  <input type="text" class="form-control" name="egg" placeholder="ไข่"  style="color: #0000FF"  required> 
                 </div>
                 <div class="col-md-5">
                 <label>ปริมาณ</label>                
                   <div class="input-group">
-                    <input type="text" class="form-control" id="eggcal" placeholder="กรัม"> 
+                    <input type="text" class="form-control" name="eggcal" placeholder="กรัม"  style="color: #0000FF"  required> 
                       <span class="input-group-btn">
-                        <button class="btn  glyphicon glyphicon-plus" type="button" id="create_eight"></button>
+                        <!-- <button class="btn  glyphicon glyphicon-plus" type="button" id="create_eight"></button> -->
+                        <button type="submit" class="btn btn-primary col-md-4 btn-block " id="create_eight">ส่ง</button>
                       </span>
                   </div>
                 </div>
               </div>
-              <span id="temp_eight"></span>
+              <!-- <span id="temp_eight"></span> -->
+      </form>
 
+      <form id="namecal">
           <div class="row">
             <div class="col-md-6">
             <label>นม</label>
-              <input type="text" class="form-control" id="milk"  placeholder="นม">
+              <input type="text" class="form-control" name="milk"  placeholder="นม"  style="color: #0000FF"  required>
             </div>
             <div class="col-md-5">
             <label>ปริมาณ</label>                
               <div class="input-group">
-                <input type="text" class="form-control" id="milkcal"  placeholder="มิลลิลิตร">
+                <input type="text" class="form-control" name="milkcal"  placeholder="มิลลิลิตร"  style="color: #0000FF"  required>
                   <span class="input-group-btn">
-                    <button class="btn  glyphicon glyphicon-plus" type="button" id="create_nine"></button>
+                    <!-- <button class="btn  glyphicon glyphicon-plus" type="button" id="create_nine"></button> -->
+                    <button type="submit" class="btn btn-primary col-md-4 btn-block " id="create_nine">ส่ง</button>
                   </span>
               </div>
             </div>
           </div>
-          <span id="temp_nine"></span>
+          <!-- <span id="temp_nine"></span> -->
+      </form>
 
+      <form id="namecal">
         <div class="row">
           <div class="col-md-6">
             <label>เครื่องเทสเครื่องปรุง</label>
-              <input type="text" class="form-control" id="seasoningmachine" placeholder="เครื่องเทสเครื่องปรุง"> 
+              <input type="text" class="form-control" name="seasoningmachine" placeholder="เครื่องเทสเครื่องปรุง"  style="color: #0000FF"  required> 
             </div>
             <div class="col-md-5">
             <label>ปริมาณ</label>                
               <div class="input-group">
-                <input type="text" class="form-control" id="seasoningmachinecal" placeholder="กรัม"> 
+                <input type="text" class="form-control" name="seasoningmachinecal" placeholder="กรัม"  style="color: #0000FF"  required> 
                   <span class="input-group-btn">
-                    <button class="btn  glyphicon glyphicon-plus" type="button" id="create_ten"></button>
+                    <!-- <button class="btn  glyphicon glyphicon-plus" type="button" id="create_ten"></button> -->
+                    <button type="submit" class="btn btn-primary col-md-4 btn-block " id="create_ten">ส่ง</button>
                   </span>
               </div>
             </div>
           </div>
-          <span id="temp_ten"></span>
-
-         
-
+          <!-- <span id="temp_ten"></span> -->
+      </form>
       </div>
     </div>
-    
   </div>
-</form>
 <br>
 
 <div class="row">
